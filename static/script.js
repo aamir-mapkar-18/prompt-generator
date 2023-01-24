a400644d9135f71e7d7815bbc2f6c9888d735331
aamir-mapkar-18 <123448636+aamir-mapkar-18@users.noreply.github.com>
2023-01-24 19:50:33 +1100

    initial commit

---
+++ static/script.js
@@ -0,0 +1,23 @@
+var progressBar = document.getElementById("progress-bar");
+var generateButton = document.getElementById("generate-button");
+
+// generateButton.addEventListener("click", function() {
+//   //disable the button
+//   generateButton.disabled = true;
+//   progressBar.style.display = "flex";
+//   var width = 0;
+//   var interval = setInterval(frame, 1000);
+
+//   function frame() {
+//     if (width >= 100) {
+//       clearInterval(interval);
+//       progressBar.style.display = "none";
+//       generateButton.disabled = false;
+//       document.querySelector(".progress-bar").style.width = '0%';
+//     } else {
+//       width += 10;
+//       document.querySelector(".progress-bar").style.width = width + "%";
+//     }
+//   }
+
+// });
